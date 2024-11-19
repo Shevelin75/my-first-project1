2eee3b16ca01affb9221e8d2e9278e19448bdc59
Shevelin75 <shevelin.oper75@gmail.com>
2024-11-19 20:27:39 +0900

    Metoduser

---
+++ METODuser/METODuser/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace METODuser
+{
+    class Program
+    {
+        // Статический метод для сложения двух чисел
+        public static int Sum(int a, int b)
+        {
+            return a + b;
+        }
+
+        // Экземплярный метод для умножения двух чисел
+        public double Multiply(double x, double y)
+        {
+            return x * y;
+        }
+
+        static void Main(string[] args)
+        {
+            // Вызов статического метода
+            int sumResult = Sum(5, 7);
+            Console.WriteLine("Сумма: " + sumResult);
+
+            // Создание экземпляра класса Program
+            Program program = new Program();
+
+            // Вызов экземплярного метода
+            double multiplyResult = program.Multiply(6.0, 8.0);
+            Console.WriteLine("Произведение: " + multiplyResult);
+
+            
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ METODuser/METODuser/User.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace METODuser
+{
+    public class User
+    {
+        // Свойства
+        public string Имя { get; set; }
+        public int Возраст { get; set; }
+        public DateTime ДатаРегистрации { get; private set; } // Только для чтения
+
+        // Конструктор
+        public User(string имя, int возраст)
+        {
+            Имя = имя;
+            Возраст = возраст;
+            ДатаРегистрации = DateTime.Now; // Устанавливается автоматически при создании пользователя
+        }
+
+        // Метод для отображения информации о пользователе
+        public void ПоказатьПрофиль()
+        {
+            Console.WriteLine($"Имя: {Имя}\nВозраст: {Возраст}\nДата регистрации: {ДатаРегистрации.ToShortDateString()}");
+        }
+    }
+}
